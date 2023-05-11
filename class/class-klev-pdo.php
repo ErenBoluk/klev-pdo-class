6f59124ad5541e7b2ab0a5b4758f028890c6effa
klevtt <eren.boluk@gappze.com>
2023-05-11 10:40:45 +0300

    Delete function and get Table name funcs added

--- class/class-klev-pdo.php
+++ class/class-klev-pdo.php
@@ -57,4 +57,31 @@ class KlevPDO
         }
         return $status;
     }
+
+    public function delete($sorgu, $parametre = array())
+    {
+        $status = false;
+        try {
+            $stmt = $this->db->prepare($sorgu);
+            $stmt->execute($parametre);
+            $status = true;
+        } catch (PDOException $e) {
+            throw new Exception("Silme hatası: " . $e->getMessage());
+        }
+        return $status;
+    }
+
+    // Required functional functions : Gerekli işlevsel fonksiyonlar
+    public function getTableName($query)
+    {
+        if (stripos($query, 'update') === 0) {
+            // Separate the table name for the UPDATE query : UPDATE sorgusu için tablo adını ayır
+            $pattern = '/\bUPDATE\s+\`?(\w+)\`?/i';
+        } else {
+            // Diğer sorgu türleri için FROM ifadesinden sonra gelen ilk kelimeyi ayır : Separate the first word after the FROM expression for other query types
+            $pattern = '/\bFROM\s+\`?(\w+)\`?/i';
+        }
+        preg_match($pattern, $query, $matches);
+        return isset($matches[1]) ? $matches[1] : null;
+    }
 }
